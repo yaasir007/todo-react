9a6666000fadc1e8a6c0187a05aab08715c79376
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-02-11 08:46:42 +0400

    fixed issue on todos display flex

--- src/myComponents/Todos.js
+++ src/myComponents/Todos.js
@@ -3,9 +3,9 @@ import { DoItem } from "./DoItem";
 
 export const Todos = (props) => {
   return (
-    <div className="container m-8">
+    <div className="m-8">
       <h3 className="mb-5 text-xl">ToDo List App</h3>
-      <div className="cards flex justify-start gap-8 flex-wrap">
+      <div className="cards flex justify-start gap-8 flex-wrap mb-32">
         {props.todos.map((todo) => {
           return (
             <DoItem todo={todo} key={todo.number} onDelete={props.onDelete} />
